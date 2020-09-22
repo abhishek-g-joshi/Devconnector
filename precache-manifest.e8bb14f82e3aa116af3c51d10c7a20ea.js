30264091ccc697eb86dfcb80b2c6f91ec53cb676
abhishek-g-joshi <ajabhishekgjoshi@gmail.com>
2020-09-22 15:22:17 +0530

    Updates

---
+++ precache-manifest.e8bb14f82e3aa116af3c51d10c7a20ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a361804e644ecf0c25cf42afe4017f",
+    "url": "/Devconnector/index.html"
+  },
+  {
+    "revision": "28e5996ac3f0a00aadb5",
+    "url": "/Devconnector/static/css/main.b0a76ff5.chunk.css"
+  },
+  {
+    "revision": "492b2c3db0bb3fbc2fdd",
+    "url": "/Devconnector/static/js/2.d16340a4.chunk.js"
+  },
+  {
+    "revision": "fe43fdc7ec11be0dca8cdf0da70fa99c",
+    "url": "/Devconnector/static/js/2.d16340a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e5996ac3f0a00aadb5",
+    "url": "/Devconnector/static/js/main.973fecfc.chunk.js"
+  },
+  {
+    "revision": "5ddb7539bd54153d081e",
+    "url": "/Devconnector/static/js/runtime-main.59814ae2.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/Devconnector/static/media/showcase.4b31330b.jpg"
+  }
+]);
